b16eacdeab93d6c42e629ea57f6658aee4f7439c
Nikolai <dolinnikolai@gmail.com>
2023-06-07 21:35:09 +0300

    task#30

--- Workshop#5/Task#28.py
+++ Workshop#5/Task#28.py
@@ -6,31 +6,38 @@
 # 2 2
 #     4
 
-def sum_1(a, b): #решение к условию задачи
-    if a == 0:
-        return b
-    return sum_1(a - 1, b + 1)
+# def sum_1(a, b): #решение к условию задачи
+#     if a == 0:
+#         return b
+#     return sum_1(a - 1, b + 1)
 
 
-# def sum_1(a, b): #не получилось доделать решение для отрицательных чисел
-#     if a < 0 and b < 0:
-#         if a == -1:
-#             return b - 1
-#         return sum_1(a + 1, b - 1)
-#     elif a < 0 and b > 0:
-#         if a == -1:
-#             return b - 1
-#         return sum_1(a + 1, b - 1)
-#     elif a > 0 and b < 0:
-#         if b == -1:
-#             return a - 1
-#         return sum_1(a - 1, b + 1)
-#     else:
-#         if a == 0:
-#             return b
-#         return sum_1(a - 1, b + 1)
+def sum_1(a, b): #не получилось доделать решение для отрицательных чисел
+    if a < 0 and b < 0:
+        if a == 0:
+            return b
+        return sum_1(a + 1, b - 1)
+    elif a < 0 < b:
+        if b == 0:
+            return a
+        return sum_1(a - 1, b - 1)
+    elif b < 0 < a:
+        if b == 0:
+            return a
+        return sum_1(a - 1, b + 1)
+    elif a > 0 and b > 0:
+        if a == 0:
+            return b
+        return sum_1(a - 1, b + 1)
 
 
 
 
-print(sum_1(int(input()),int(input())))
+# print(sum_1(2, 3))
+# print(sum_1(3, 2))
+# print(sum_1(-2, -3))
+# print(sum_1(-3, -2))
+print(sum_1(-2, 3))
+# print(sum_1(2, -3))
+# print(sum_1(3, -2))
+# print(sum_1(-3, 2))
